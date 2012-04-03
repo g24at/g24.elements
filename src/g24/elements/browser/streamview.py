b24a3433b759b3437c87914ecbb19e70d2c6559c
Johannes Raggam <raggam-nl@adm.at>
2012-04-03 15:22:30 +0200

    add filters

--- src/g24/elements/browser/streamview.py
+++ src/g24/elements/browser/streamview.py
@@ -4,9 +4,15 @@ from g24.elements.content import IBasetype
 
 class StreamView(BrowserView):
 
-    def items(self):
+    def items(self, user=None, tag=None):
         # TODO: get parameters from request or pass as arguments
-        
+        #
+
+        if not user and 'user' in self.request.form:
+            user = self.request.form['user']
+        if not tag and 'tag' in self.request.form:
+            tag = self.request.form['tag']
+
         cat = getToolByName(self.context, 'portal_catalog')
 
         query = {}
@@ -15,5 +21,11 @@ class StreamView(BrowserView):
         query['sort_on'] = 'created'
         query['sort_order'] = 'reverse'
 
+        if user:
+            query['Creator'] = user
+        if tag:
+            query['Subject'] = tag
+
         result = cat(**query)
+
         return result
