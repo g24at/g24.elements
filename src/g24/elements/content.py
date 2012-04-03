41d7331e31b21306be2674e864161c5690bd34c0
Johannes Raggam <raggam-nl@adm.at>
2012-04-03 20:58:15 +0200

    rename threadview -> streamview. add bareform for experimenting with forms

--- src/g24/elements/content.py
+++ src/g24/elements/content.py
@@ -1,6 +1,9 @@
 from plone.directives import form
+from z3c.form import field
 from plone.app.textfield import RichText
 from g24.elements import messageFactory as _
+
+from plone.z3cform import layout
 
 class IBasetype(form.Schema):
     """A folder that can contain cinemas
@@ -11,3 +14,8 @@ class IBasetype(form.Schema):
             description=_(u"Main text of this content node."),
             required=False
         )
+
+class BasetypeForm(form.Form):
+    fields = field.Fields(IBasetype)
+
+BasetypeFormView = layout.wrap_form(BasetypeForm)
