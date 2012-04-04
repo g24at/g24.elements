c0100587ad00e47605057ddfb9c4ad5bbb774b9b
Johannes Raggam <raggam-nl@adm.at>
2012-04-04 13:44:29 +0200

    form fixes. stub.

--- src/g24/elements/content.py
+++ src/g24/elements/content.py
@@ -1,5 +1,5 @@
 from plone.directives import form
-from z3c.form import field
+from z3c.form import field, button
 from plone.app.textfield import RichText
 from g24.elements import messageFactory as _
 
@@ -18,4 +18,21 @@ class IBasetype(form.Schema):
 class BasetypeForm(form.Form):
     fields = field.Fields(IBasetype)
 
+    @button.buttonAndHandler(u'Submit')
+    def handleApply(self, action):
+        data, errors = self.extractData()
+        # do something
+
 BasetypeFormView = layout.wrap_form(BasetypeForm)
+"""
+class BasetypeFormView(layout.FormWrapper):
+
+    def __init__(self, request, context):
+        self.request = request
+        self.context = context
+
+    def __call__(self):
+        context = self.context
+        import pdb;pdb.set_trace()
+        return layout.wrap_form(BasetypeForm)(self.context, self.request).__call__()
+"""
