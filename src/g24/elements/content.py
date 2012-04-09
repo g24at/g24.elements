4c8efb966857de7237e6a420ddc676cbc519fa51
Johannes Raggam <raggam-nl@adm.at>
2012-04-09 22:56:49 +0200

    add title and richtext behaviors

--- src/g24/elements/content.py
+++ src/g24/elements/content.py
@@ -1,14 +1,5 @@
 from plone.directives import form
-from plone.app.textfield import RichText
-from g24.elements import messageFactory as _
-
 
 class IBasetype(form.Schema):
-    """A folder that can contain cinemas
+    """ g24.elements Basetype
     """
-
-    text = RichText(
-            title=_(u"Body text"),
-            description=_(u"Main text of this content node."),
-            required=False
-        )
