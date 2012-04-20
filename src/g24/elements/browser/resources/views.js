dcb81349c94b5f77ecaf9b3dbaf3cfffa8626d1c
Johannes Raggam <raggam-nl@adm.at>
2012-04-20 23:36:45 +0200

    fix sharingbox insertion. doesnt work with FRAGMENTS!!! have to adapt the pagetemplate to not deliver html head and sutff. hmpf.

--- src/g24/elements/browser/resources/views.js
+++ src/g24/elements/browser/resources/views.js
@@ -3,12 +3,17 @@
     /*
      * SHARINGBOX INTEGRATION
      * */
+    var last_context=null;
     function sharingbox_inserter(linkel, event) {
         event.preventDefault();
         $('#sharingbox').remove(); // remove first any sharingbox instance
-        //$(linkel).parent().parent().hide(); 
-        $(linkel).parent().parent().load($(linkel).attr('href') + ' #sharingbox');
-
+        var context = $(linkel).parent().parent();
+        if (last_context !== null) { $('#'+last_context).show(); }
+        last_context = context.attr('id');
+        context.hide();
+        $.get($(linkel).attr('href'), function(data){
+            context.after($(data));
+        });
         /*
         $.ajax({
           dataType : 'html',
