72df36657989aa4933d29ccc8cee79d2211d02c9
Johannes Raggam <raggam-nl@adm.at>
2012-04-28 03:46:47 +0200

    better sharingbox inserter with proper replacing of the article or adding a li within an existant or new ul

--- src/g24/elements/browser/resources/views.js
+++ src/g24/elements/browser/resources/views.js
@@ -4,25 +4,43 @@
 (function ($) {
 
     var last_context=null;
-    function sharingbox_inserter(linkel, event) {
-        event.preventDefault();
+    function sharingbox_remove() {
         $('#sharingbox').remove(); // first remove any sharingbox instance
-        var context = $(linkel).parent().parent();
+        $('#sharingbox_ul_wrapper').remove(); // first remove any sharingbox instance
+        $('#sharingbox_li_wrapper').remove(); // first remove any sharingbox instance
         if (last_context !== null) { $('#'+last_context).show(); }
-        last_context = context.attr('id');
-        context.hide();
+    }
+    function sharingbox_inserter(linkel, event, add) {
+        event.preventDefault();
+        sharingbox_remove();
+
+        var context = $(linkel).parent().parent();
+        var context_id = context.attr('id');
+        last_context = context_id;
+        /* ajax get */
         $.get($(linkel).attr('href'), function(data){
-            context.after($(data));
+            if (add===true) {
+                if ($(context_id + ' ul').length) {
+                    $(context_id + ' ul').before('<li id="sharingbox_li_wrapper"></li>');
+                } else {
+                    context.after('<ul id="sharingbox_ul_wrapper"><li id="sharingbox_li_wrapper"></li></ul>');
+                }
+                $('#sharingbox_li_wrapper').html($(data));
+            }
+            else {
+                context.hide();
+                context.after($(data));
+            }
             sharingbox_init();
         });
     }
 
     $(document).ready(function() {
         $('a.sharingbox_edit').click(function(event){
-          sharingbox_inserter(this, event);
+          sharingbox_inserter(this, event, false);
         });
         $('a.sharingbox_add').click(function(event){
-          sharingbox_inserter(this, event);
+          sharingbox_inserter(this, event, true);
         });
     });
 
