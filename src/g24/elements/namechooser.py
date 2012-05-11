9516b970037f36c870135e597f2f1659e29fe01a
Johannes Raggam <raggam-nl@adm.at>
2012-05-11 18:35:57 +0200

    integer namechooser

---
+++ src/g24/elements/namechooser.py
@@ -0,0 +1,38 @@
+from Acquisition import aq_inner
+from Products.CMFCore.utils import getToolByName
+from zope.component.hooks import getSite
+from zope.container.interfaces import INameChooser
+from zope.interface import implements
+import transaction
+
+class ElementIdChooser(object):
+    """A name chooser for a G24 elements.
+
+    """
+    implements(INameChooser)
+
+    def __init__(self, context):
+        self.context = context
+
+    def checkName(self, name, object):
+        context = aq_inner(self.context)
+        cat = getToolByName(context, 'portal_catalog')
+        res = cat.searchResults(id=name)
+        if res:
+            raise ValueError("Name not unique.")
+        else:
+            return True
+
+    def chooseName(self, name, object):
+
+        # get/set maxid
+        site = getSite()
+        g24element_max_id = getattr(site, 'g24element_max_id', None)
+        if g24element_max_id == None:
+            g24element_max_id = site.g24element_max_id = 1
+        else:
+            g24element_max_id += 1
+            site.g24element_max_id = g24element_max_id
+            transaction.commit() # TODO: thread safety?
+
+        return str(g24element_max_id)
