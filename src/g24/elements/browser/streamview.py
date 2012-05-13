12da1c7a192ed9fe407c49c7944ccf7026b7f10f
Johannes Raggam <raggam-nl@adm.at>
2012-05-14 01:24:33 +0200

    filter on type and path

--- src/g24/elements/browser/streamview.py
+++ src/g24/elements/browser/streamview.py
@@ -6,7 +6,7 @@ from g24.elements.content import IBasetype
 
 class StreamView(BrowserView):
 
-    def items(self, user=None, tag=None):
+    def items(self, user=None, tag=None, path=False, type_=None):
         # TODO: get parameters from request or pass as arguments
         #
 
@@ -14,15 +14,28 @@ class StreamView(BrowserView):
             user = self.request.form['user']
         if not tag and 'tag' in self.request.form:
             tag = self.request.form['tag']
+        if not path and 'path' in self.request.form:
+            path = self.request.form['path']
+
+        if not type_ and 'type' in self.request.form:
+            type_ = self.request.form['type']
+        if type_:
+            if type_.lower() == 'event':
+                type_ = 'plone.app.event.interfaces.IEvent'
 
         cat = getToolByName(self.context, 'portal_catalog')
 
         query = {}
-        query['object_provides'] = IBasetype.__identifier__
+        if type_:
+            query['object_provides'] = type_
+        else:
+            query['object_provides'] = IBasetype.__identifier__
 
         query['sort_on'] = 'created'
         query['sort_order'] = 'reverse'
 
+        if path:
+            query['path'] = {'query': '/'.join(self.context.getPhysicalPath())}
         if user:
             query['Creator'] = user
         if tag:
