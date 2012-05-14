70d5dfb8eff2315abf72024fb58f1da7ece71d6c
Johannes Raggam <raggam-nl@adm.at>
2012-05-14 04:00:18 +0200

    add solgema fullcalendar, html layout fix, visual css thread marker in threadview

--- src/g24/elements/config.py
+++ src/g24/elements/config.py
@@ -15,7 +15,7 @@ EVENT_BEHAVIORS = ('plone.app.event.dx.behaviors.IEventBasic',
                    'plone.app.event.dx.behaviors.IEventRecurrence',
                    'plone.app.event.dx.behaviors.IEventLocation')
 
-TITLE_INTERFACES =  (ITitle)
-TITLE_BEHAVIORS = ('g24.elements.behaviors.ITitle')
-PLACE_INTERFACES = (IPlace)
-PLACE_BEHAVIORS = ('g24.elements.behaviors.IPlace')
+TITLE_INTERFACES =  (ITitle,)
+TITLE_BEHAVIORS = ('g24.elements.behaviors.ITitle',)
+PLACE_INTERFACES = (IPlace,)
+PLACE_BEHAVIORS = ('g24.elements.behaviors.IPlace',)
