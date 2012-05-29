b8b1d2dda96a99fc67b234e7fdd5b671de7c39f3
Johannes Raggam <raggam-nl@adm.at>
2012-05-29 14:09:08 +0200

    merge back again zmo changes

--- src/g24/elements/sharingbox/resources/sharingbox.js
+++ src/g24/elements/sharingbox/resources/sharingbox.js
@@ -2,9 +2,10 @@
 
     var EDIT = 0;
     var ADD = 1;
-    var last_context = null;
-
-    var shbx_lock = 0;  // lock-counter to prevent multiple loading of sharingboxes
+    var last_context=null;
+    
+    var shbx_lock   = 0;  // lock-counter to prevent multiple loading of sharingboxes
+    var shbx_dirty  = 0;  // isDirty marker, set in onchange-handlers 
 
     /* TOols
      * */
@@ -67,10 +68,17 @@
          * */
         event.preventDefault();
 
-        // test for and set lock
-        if (shbx_lock > 0) { return; }
+        // test for "create-lock" (prevent double creation)
+        if (shbx_lock > 0 ) { return; }
+        
+        // test for dirty inputs
+        if ( isInputDirty() ) {
+            if ( ! confirm('Unsaved changes - really discard ?') ) { return; } 
+        }
+        
+        // set "create lock" 
         shbx_lock++;
-
+        
         sharingbox_remove();
         if (last_context !== null) { $('#' + last_context).show(); }
 
@@ -212,6 +220,8 @@
         editor.observe("load", function () {
             $(this.composer.iframe).autoResize();
         });
+        
+        editor.observe("change", function(){shbx_dirty=1;});
 
 
         $('.datepicker').dateinput({
@@ -260,7 +270,25 @@
                     sharingbox_enable();
                 }
             );
+            
+            shbx_dirty = 0; // submitted, reset dirty marker
         });
+        
+        // set onchange handlers
+        $("#sharingbox>form input").change(function(){shbx_dirty=1;});
+        $("#sharingbox>form select").change(function(){shbx_dirty=1;});
+        
+        $(window).on('beforeunload',function(){
+            if ( isInputDirty() ) return false;
+        });
+    }
+    
+    /*
+     * Dirty Input
+     */
+    function isInputDirty()
+    {
+        return (shbx_dirty > 0);
     }
 
 
