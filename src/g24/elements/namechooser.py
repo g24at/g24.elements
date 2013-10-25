bcb19a071e3791b22473f25c23de862c35e2479b
Johannes Raggam <raggam-nl@adm.at>
2013-10-25 23:02:22 +0200

    pep8, subtransactions

--- src/g24/elements/namechooser.py
+++ src/g24/elements/namechooser.py
@@ -5,9 +5,9 @@ from zope.container.interfaces import INameChooser
 from zope.interface import implements
 import transaction
 
+
 class ElementIdChooser(object):
     """A name chooser for a G24 elements.
-
     """
     implements(INameChooser)
 
@@ -24,16 +24,15 @@ class ElementIdChooser(object):
             return True
 
     def chooseName(self, name, object):
-
         # get/set maxid
         site = getSite()
         g24element_max_id = getattr(site, 'g24element_max_id', None)
-        if g24element_max_id == None:
+        if g24element_max_id is None:
             g24element_max_id = site.g24element_max_id = 1
         else:
             g24element_max_id += 1
             site.g24element_max_id = g24element_max_id
-            transaction.commit() # TODO: thread safety?
+            transaction.get().commit()  # TODO: thread safety?
             #transaction.commit(1) # subtransaction commit
 
         return str(g24element_max_id)
