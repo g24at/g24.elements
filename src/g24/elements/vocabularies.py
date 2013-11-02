0836dc546c1dc46921b314af9ed5ad853e52fb68
Johannes Raggam <raggam-nl@adm.at>
2013-11-02 16:05:44 +0100

    make locationsvocab queryable

--- src/g24/elements/vocabularies.py
+++ src/g24/elements/vocabularies.py
@@ -5,16 +5,19 @@ from zope.schema.vocabulary import SimpleVocabulary
 from g24.elements.behaviors import IPlace
 from zope.interface import directlyProvides
 from Products.CMFPlone.utils import safe_unicode
+from zope.component.hooks import getSite
 
 
-def LocationsVocabulary(context):
+def LocationsVocabulary(context, query=None):
     """Vocabulary factory for countries regarding to ISO3166.
     """
-    cat = getToolByName(context, 'portal_catalog')
-    query = {}
-    query['object_provides'] = IPlace.__identifier__
-    query['sort_on'] = 'sortable_title'
+    cat = getToolByName(getSite(), 'portal_catalog')
+    cat_query = {}
+    cat_query['object_provides'] = IPlace.__identifier__
+    cat_query['sort_on'] = 'sortable_title'
     items = [SimpleTerm(value=it.UID, title=safe_unicode(it.Title))
-             for it in cat(**query)]
+             for it in cat(**cat_query)
+             if query is None
+             or safe_unicode(query).lower() in safe_unicode(it.Title).lower()]
     return SimpleVocabulary(items)
 directlyProvides(LocationsVocabulary, IVocabularyFactory)
